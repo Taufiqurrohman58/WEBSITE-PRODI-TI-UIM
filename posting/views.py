f8954eda88105bf5c0b1eed2244a113ce84d3f15
Taufiqurrohman58 <taufiqurrohman250503@gmail.com>
2023-12-27 14:18:51 -0800

    membuat isi menu berita dan membuat halaman baru semua berita

--- posting/views.py
+++ posting/views.py
@@ -37,6 +37,90 @@ def berita(request):
     template = loader.get_template('berita.html')
     return HttpResponse(template.render())
 
+def berita1(request):
+    template = loader.get_template('berita1.html')
+    return HttpResponse(template.render())
+
+def berita2(request):
+    template = loader.get_template('berita2.html')
+    return HttpResponse(template.render())
+
+def berita3(request):
+    template = loader.get_template('berita3.html')
+    return HttpResponse(template.render())
+
+def berita4(request):
+    template = loader.get_template('berita4.html')
+    return HttpResponse(template.render())
+
+def berita5(request):
+    template = loader.get_template('berita5.html')
+    return HttpResponse(template.render())
+
+def berita6(request):
+    template = loader.get_template('berita6.html')
+    return HttpResponse(template.render())
+
+def berita7(request):
+    template = loader.get_template('berita7.html')
+    return HttpResponse(template.render())
+
+def berita8(request):
+    template = loader.get_template('berita8.html')
+    return HttpResponse(template.render())
+
+def berita9(request):
+    template = loader.get_template('berita9.html')
+    return HttpResponse(template.render())
+
+def berita10(request):
+    template = loader.get_template('berita10.html')
+    return HttpResponse(template.render())
+
+def berita2(request):
+    template = loader.get_template('berita2.html')
+    return HttpResponse(template.render())
+
+def berita3(request):
+    template = loader.get_template('berita3.html')
+    return HttpResponse(template.render())
+
+def berita4(request):
+    template = loader.get_template('berita4.html')
+    return HttpResponse(template.render())
+
+def berita5(request):
+    template = loader.get_template('berita5.html')
+    return HttpResponse(template.render())
+
+def berita6(request):
+    template = loader.get_template('berita6.html')
+    return HttpResponse(template.render())
+
+def berita7(request):
+    template = loader.get_template('berita7.html')
+    return HttpResponse(template.render())
+
+def berita8(request):
+    template = loader.get_template('berita8.html')
+    return HttpResponse(template.render())
+
+def berita9(request):
+    template = loader.get_template('berita9.html')
+    return HttpResponse(template.render())
+
+def berita10(request):
+    template = loader.get_template('berita10.html')
+    return HttpResponse(template.render())
+
+def beritapage2(request):
+    template = loader.get_template('berita-page2.html')
+    return HttpResponse(template.render())
+
+def beritapage3(request):
+    template = loader.get_template('berita-page3.html')
+    return HttpResponse(template.render())
+
 def jadwaluas(request):
     template = loader.get_template('jadwaluas.html')
     return HttpResponse(template.render())
